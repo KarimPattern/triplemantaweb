191c6a9478aaef28505b5b865a0c151d6408909d
DarthKarim <darthkarim@proton.me>
2026-03-04 14:03:36 +0100

    Reverted base changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { githubPagesSpa } from '@sctg/vite-plugin-github-pages-spa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/triplemantaweb/',
+  base: '/',
   plugins: [
     react(),
     githubPagesSpa({ verbose: true }),
